3f77ada8dc87c903c896ef28024efd41ac8505a4
Cadtamas <cadtamas@gmail.com>
2017-12-31 10:38:03 +0100

    Using SpawnActor<>() to Spawn
    -Replaced the projectile with a sphere object
    -Create a new ProjecileBlueprint reference in the Tank.h (TSubclassOf<AProjectile> ProjectileBlueprint;)
    -In the Tank_BP choose the Projectile_BP under the Setup->Projectile Blueprint
    
    -We will need a barrel reference too (idk why) UTankBarrel* Barrel = nullptr;
    -In the Tank.cpp, we make a local reference in the SetBarrelReference... Barrel = BarrelToSet;
    -Protect it in the fire method. (if (!Barrel) { return; })
    -Spawn a projectile in the barrel's projectile socket
    GetWorld()->SpawnActor<AProjectile>(
    		ProjectileBlueprint,
    		Barrel->GetSocketLocation(FName ("Projectile")),
    		Barrel->GetSocketRotation(FName("Projectile"))
    		)
    
    -Includes:
    #include "Tank.h"
    #include "TankBarrel.h"
    #include "Projectile.h"
    #include "TankAimingComponent.h"

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -7,8 +7,8 @@
 #include "Tank.generated.h"
 
 class UTankBarrel;
-class UTankBarrel;
 class UTankAimingComponent;
+class AProjectile;
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -41,6 +41,10 @@ private:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	UPROPERTY(EditAnywhere, Category = Firing)
-		float LaunchSpeed = 4000; 
+	float LaunchSpeed = 4000; 
 	
+	UPROPERTY(EditAnywhere, Category = Setup)
+	TSubclassOf<AProjectile> ProjectileBlueprint; //Projectile reference
+
+	UTankBarrel* Barrel = nullptr; //Local barrel reference for spawning projectile
 };
