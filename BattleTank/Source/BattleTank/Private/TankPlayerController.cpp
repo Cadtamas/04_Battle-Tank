595c1da05e1eef11da1c6039e5da764fac1338a7
Cadtamas <cadtamas@gmail.com>
2018-01-08 19:58:09 +0100

    BlueprintImplementableEvent
    -in the  TankPlayerController.h make a new method
    FoundAimingComponent
    -Call it in the TankPlayerController bp's eventgraph
    -Call the function in th TankPlayerController.cpp's BeginPlay
    -In the TankPlayerController bp delete the Get ControlledTank; Get component by Class; Get; Event BeginPlay nodes...
    -Connect  the EventFoundAimingComp output exec to the Create Player UI widget input exec, and connect EventFoundAimingComp's Aim Comp ref to the set

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankPlayerController.h"
+#include "TankAimingComponent.h"
 #include "Tank.h"
 
 //Tick
@@ -17,7 +18,15 @@ void ATankPlayerController::Tick(float DeltaTime)
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
-
+	auto AimingComponent = GetControlledTank()->FindComponentByClass<UTankAimingComponent>();
+	if (AimingComponent)
+	{
+		FoundAimingComponent(AimingComponent);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player controller can't find aiming component at BeginPlay"))
+	}
 }
 
 
