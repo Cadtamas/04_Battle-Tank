a6b846a13a81fa86c2b4d6c97528f27f76f7f871
Cadtamas <cadtamas@gmail.com>
2018-01-09 20:35:29 +0100

    Starting From Green
    The goal is to temove the Tank.h/cpp from the project.
    -First of all we have to fix the aiming issues (missing components)
    we need the tainkaimingcomponent in the tank.cpp
    TankAimingComponent = FindComponentByClass<UTankAimingComponent>()

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -31,6 +31,7 @@ private:
 
 	virtual void BeginPlay() override;
 
+	//TODO remove once firing is moved to aiming component
 	UPROPERTY(EditDefaultsOnly, Category = "Firing")
 	float LaunchSpeed = 4000; 
 
