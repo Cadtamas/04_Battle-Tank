c68a42b23c4cb388387a2ff046a8fae88a31af94
Cadtamas <cadtamas@gmail.com>
2018-01-13 17:47:45 +0100

    Tweaking Tank AI
    ---Improve aiming---
    -In the Tank_BP->Tank aiming->Details->Firing->Launch Speed=20000
    -If we aiming an untracaeable object, the aiming works weird. We would like to change it to stop the aiming if we aim at the sky btw.
    Change the C++ TankPlayerController's GetSightRayHitLocation method to return false if we don't aim at any object and return true if we aim at one...
    -We want tho mobe the barrel the shortest way to the target, so we change the TankAimingComonent's  MoveBarrelTowards method (we change the direction if the angle is bigger than 180°)

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -30,7 +30,8 @@ void ATankPlayerController::AimTowardsCrosshair()
 	if (!ensure(AimingComponent)) { return; }
 
 	FVector HitLocation; // Out parameter
-	if (GetSightRayHitLocation(HitLocation))  //Has "side-effect", is going to line trace
+	bool bGotHitLocation = GetSightRayHitLocation(HitLocation);
+	if (bGotHitLocation)  //Has "side-effect", is going to line trace
 	{
 		AimingComponent->AimAt(HitLocation);
 		//TODO Tell controlled tank to aim at this point
@@ -50,10 +51,10 @@ bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) cons
 	if (GetLookDirection(ScreenLocation, LookDirection))
 	{
 		//Line-trace along that look direction, and see what we hit (Up to maximum range)
-		GetLookVectorHitLocation(LookDirection, OutHitLocation);
+		return GetLookVectorHitLocation(LookDirection, OutHitLocation);
 	}
 	
-	return true;
+	return false;
 }
 
 bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const
